de01cd5328bc419b5e377e00a18b4e452cf231cb
joaobessada <joaobessada@gmail.com>
2022-12-10 07:44:29 -0300

    first commit

---
+++ 01-SpringBootCore/src/main/java/com/bessada/springbootexpert/SpringbootExpertApplication.java
@@ -0,0 +1,13 @@
+package com.bessada.springbootexpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootExpertApplication.class, args);
+	}
+
+}
