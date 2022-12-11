c51c79d3ab8b3502627a1bd91bd0d3d8b6a8f33b
joaobessada <joaobessada@gmail.com>
2022-12-11 10:45:51 -0300

    02 - crud

--- 02-SpringBootJPA/src/main/java/com/bessada/springbootexpert/domain/repository/Clientes.java
+++ 02-SpringBootJPA/src/main/java/com/bessada/springbootexpert/domain/repository/Clientes.java
@@ -14,14 +14,38 @@ import java.util.List;
 @Repository
 public class Clientes {
 
-    private static String INSERT = "insert into cliente (nome) values (?)";
-    private static String SELECT_ALL = "SELECT * FROM CLIENTE";
+    private static String INSERT = "insert into cliente (nome) values (?) ";
+    private static String SELECT_ALL = "SELECT * FROM CLIENTE ";
+    private static String UPDATE = "update cliente set nome = ? where id = ? ";
+    private static String DELETE = "delete from cliente where id = ? ";
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public Cliente salvar(Cliente cliente) {
-        jdbcTemplate.update( INSERT, new Object[]{cliente.getNome()});
+    public Cliente salvar(Cliente cliente){
+        jdbcTemplate.update( INSERT, new Object[]{cliente.getNome()} );
         return cliente;
+    }
+
+    public Cliente atualizar(Cliente cliente){
+        jdbcTemplate.update(UPDATE, new Object[]{
+                cliente.getNome(), cliente.getId()} );
+        return cliente;
+    }
+
+    public void deletar(Cliente cliente){
+        deletar(cliente.getId());
+    }
+
+    public void deletar(Integer id){
+        jdbcTemplate.update(DELETE, new Object[]{id});
+    }
+
+    public List<Cliente> buscarPorNome(String nome){
+        return jdbcTemplate.query(
+                SELECT_ALL.concat(" where nome like ? "),
+                new Object[]{"%" + nome + "%"},
+                obterClienteMapper());
     }
 
     public List<Cliente> obterTodos(){
