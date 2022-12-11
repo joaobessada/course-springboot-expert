e686ab08d538fe182881e8029522c7199426dec4
joaobessada <joaobessada@gmail.com>
2022-12-11 12:06:17 -0300

    02 - to jdbctemplate to jpa

--- 02-SpringBootJPA/src/main/java/com/bessada/springbootexpert/domain/entity/Cliente.java
+++ 02-SpringBootJPA/src/main/java/com/bessada/springbootexpert/domain/entity/Cliente.java
@@ -1,7 +1,17 @@
 package com.bessada.springbootexpert.domain.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="cliente")
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Integer id;
+
+    @Column(name="nome", length=100)
     private String nome;
 
     public Cliente() {}
