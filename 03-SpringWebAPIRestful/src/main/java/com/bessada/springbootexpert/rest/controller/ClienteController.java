dbde646b098373b47a9ae34206403dd0abff34bc
joaobessada <joaobessada@gmail.com>
2022-12-11 22:17:41 -0300

    03 - find method custom/first way done

--- 03-SpringWebAPIRestful/src/main/java/com/bessada/springbootexpert/rest/controller/ClienteController.java
+++ 03-SpringWebAPIRestful/src/main/java/com/bessada/springbootexpert/rest/controller/ClienteController.java
@@ -3,10 +3,13 @@ package com.bessada.springbootexpert.rest.controller;
 import com.bessada.springbootexpert.domain.entity.Cliente;
 import com.bessada.springbootexpert.domain.repository.Clientes;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -51,6 +54,18 @@ public class ClienteController {
                     clientes.save(cliente);
                     return ResponseEntity.noContent().build();
         }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity find(Cliente filtro) {
+        ExampleMatcher matcher = ExampleMatcher
+                                    .matching()
+                                    .withIgnoreCase()
+                                    .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+
+        Example example = Example.of(filtro, matcher);
+        List<Cliente> lista = clientes.findAll(example);
+        return ResponseEntity.ok(lista);
     }
 
 }
