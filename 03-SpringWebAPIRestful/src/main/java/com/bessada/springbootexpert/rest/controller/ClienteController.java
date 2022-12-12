d689b0b14c78da664b33b25fefe27edd52dfb6ae
joaobessada <joaobessada@gmail.com>
2022-12-11 21:56:34 -0300

    03 - delete and put

--- 03-SpringWebAPIRestful/src/main/java/com/bessada/springbootexpert/rest/controller/ClienteController.java
+++ 03-SpringWebAPIRestful/src/main/java/com/bessada/springbootexpert/rest/controller/ClienteController.java
@@ -31,5 +31,26 @@ public class ClienteController {
         return ResponseEntity.ok(clienteSalvo);
     }
 
+    @DeleteMapping(value = "/{id}")
+    @ResponseBody
+    public ResponseEntity delete(@PathVariable Integer id) {
+        Optional<Cliente> cliente = clientes.findById(id);
+        if(cliente.isPresent()) {
+            clientes.delete(cliente.get());
+            return ResponseEntity.noContent().build();
+        }else return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity update(@PathVariable Integer id, @RequestBody Cliente cliente) {
+        return clientes
+                .findById(id)
+                .map(clienteExistente -> {
+                    cliente.setId(id);
+                    clientes.save(cliente);
+                    return ResponseEntity.noContent().build();
+        }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
 }
