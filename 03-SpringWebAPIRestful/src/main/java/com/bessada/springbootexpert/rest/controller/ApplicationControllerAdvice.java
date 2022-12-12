4ae6f1d740827ae5c915e0207d1b81f693e5bcc5
joaobessada <joaobessada@gmail.com>
2022-12-12 10:59:11 -0300

    03 - controlleradvice/exceptionhandler

---
+++ 03-SpringWebAPIRestful/src/main/java/com/bessada/springbootexpert/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,19 @@
+package com.bessada.springbootexpert.rest.controller;
+
+import com.bessada.springbootexpert.exception.RegraNegocioException;
+import com.bessada.springbootexpert.rest.ApiErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleRegraNegocioException(RegraNegocioException ex) {
+        String mensagemErro = ex.getMessage();
+        return new ApiErrors(mensagemErro);
+    }
+}
--- 03-SpringWebAPIRestful/src/main/java/com/bessada/springbootexpert/rest/controller/PedidoController.java
+++ 03-SpringWebAPIRestful/src/main/java/com/bessada/springbootexpert/rest/controller/PedidoController.java
@@ -26,5 +26,4 @@ public class PedidoController {
         Pedido pedido = service.salvar(dto);
         return pedido.getId();
     }
-
 }
